714250bde48e717bb60999b3730d05a19d72b4d6
Tomislav Galić <tomislav.galic19@gmail.com>
2020-09-11 23:04:16 +0200

    Commit

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -28,6 +28,8 @@ class QuestionController extends AbstractController
             'Maybe... try spell backwars',
         ];
 
+        dump($this);
+
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace(' ', ' ', $slug)),
